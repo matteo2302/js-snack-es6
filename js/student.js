aa8ede9eb256f3149902f7d0f75b77b2201063ad
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-06-13 16:17:07 +0200

    students?

--- js/student.js
+++ js/student.js
@@ -1,1 +1,51 @@
 console.log("jsok");
+const students = [
+  {
+    id: 213,
+    name: "Marco della Rovere",
+    grades: 78,
+  },
+  {
+    id: 110,
+    name: "Paola Cortellessa",
+    grades: 96,
+  },
+  {
+    id: 250,
+    name: "Andrea Mantegna",
+    grades: 48,
+  },
+  {
+    id: 145,
+    name: "Gaia Borromini",
+    grades: 74,
+  },
+  {
+    id: 196,
+    name: "Luigi Grimaldello",
+    grades: 68,
+  },
+  {
+    id: 102,
+    name: "Piero della Francesca",
+    grades: 50,
+  },
+  {
+    id: 120,
+    name: "Francesca da Polenta",
+    grades: 84,
+  },
+];
+
+const betterThan70 = students.filter((student) => {
+  return student.grades > 70;
+});
+console.log(betterThan70);
+const betterThan70idmore120 = students.filter((student) => {
+  return student.grades > 70 && student.id > 120;
+});
+console.log(betterThan70idmore120);
+const names = students.map((student) => {
+  return student.name.toUpperCase();
+});
+console.log(names);
--- readme.md
+++ readme.md
@@ -2,3 +2,11 @@
 
 - creiamo l'array con i nomi
   -creiamo un ciclo che giri dentro e per ogni valore crei un array con nome numero di tavolo e nome del tavolo
+
+# students
+
+- creo l'array di students e gli inserisco oggetti con i loro valori come nome id e voti
+- creo un ciclo filter dove filtro tutti gli studenti con un voto supeririore a 70
+- creo un ciclo filter dove filtro tutti gli studenti con un voto supeririore a 70 e id superiore a 120
+-
+- creo un lista dove ci sono solo i nomi in masiuscolo

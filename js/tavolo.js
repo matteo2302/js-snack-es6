ab0d33f796b92a91ccde9b1f16785681b746c148
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-06-13 15:18:40 +0200

    first commit

---
+++ js/tavolo.js
@@ -0,0 +1,23 @@
+console.log("jsok");
+const invitati = [
+  "Brad Pitt",
+  "Johnny Depp",
+  "Lady Gaga",
+  "Cristiano Ronaldo",
+  "Georgina Rodriguez",
+  "Chiara Ferragni",
+  "Fedez",
+  "George Clooney",
+  "Amal Clooney",
+  "Maneskin",
+];
+// let tavoloinvitati = [];
+// for (let invitato of invitati) {
+//   invitato = {
+//     nome: [invitato],
+//     numeroTavolo: [invitato],
+//     nomeDelTavolo: [invitato],
+//   };
+//   tavoloinvitati.push(invitato);
+// }
+// console.table(tavoloinvitati);
---
+++ readme.md
@@ -0,0 +1,4 @@
+# invitati di Gatsby
+
+- creiamo l'array con i nomi
+  -creiamo un ciclo che giri dentro e per ogni valore crei un array con nome numero di tavolo e nome del tavolo

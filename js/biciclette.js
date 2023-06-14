1f71291c6b3cdf56b5037f488ed5b8314cb19ac0
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-06-14 15:41:09 +0200

    cannot change light bicycle

--- js/biciclette.js
+++ js/biciclette.js
@@ -1,1 +1,49 @@
 console.log("JSOK");
+const bicycles = [
+  {
+    name: "Gianfranco",
+    weight: 30,
+  },
+  {
+    name: "Pierluigi",
+    weight: 38,
+  },
+  {
+    name: "Rachele",
+    weight: 35,
+  },
+  {
+    name: "Benedetta",
+    weight: 50,
+  },
+  {
+    name: "Marcella",
+    weight: 70,
+  },
+  {
+    name: "Pietrina",
+    weight: 65,
+  },
+  {
+    name: "Matteo",
+    weight: 80,
+  },
+];
+
+const { name, weight } = bicycles;
+
+let lightbicycle = 100;
+for (i = 0; i < bicycles.length; i++) {
+  const bicycle = weight;
+  if (bicycle < weight) {
+    lightbicycle = weight;
+  }
+}
+console.log(lightbicycle);
+// const fatBibycle = bicycles.forEach((weight) => {
+//   fatBibycle = weight;
+//   if (fatBibycle > weight) {
+//     fatBibycle = weight;
+//   }
+// });
+// console.log(fatBibycle);
